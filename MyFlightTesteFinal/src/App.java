5b413db0b02c88690e10217d3e140e42b705aacf
16111077 <16111077@W1847409.portoalegre.pucrsnet.br>
2017-08-30 18:47:59 -0300

    LAÇO PARA PESQUISAS DE NOME DA CIA AEREA SOBRE OBJETOS DE ARRAYLIST FUNCIONANDO

--- MyFlightTesteFinal/src/App.java
+++ MyFlightTesteFinal/src/App.java
@@ -13,18 +13,28 @@ public class App {
 		CiaAerea companhia3 = new CiaAerea("VARIG", "POA", "BH");
 		CiaAerea companhia4 = new CiaAerea("AZUL", "POA", "CANOAS");
 		CiaAerea companhia5 = new CiaAerea("TWA", "POA", "NEW YORK");
+		CiaAerea companhia6 = new CiaAerea("TAM", "POA", "SP");
 
 		Lista.add(0, companhia);
 		Lista.add(1, companhia2);
 		Lista.add(2, companhia3);
 		Lista.add(3, companhia4);
 		Lista.add(4, companhia5);
+		Lista.add(5, companhia6);
 
 		for (int i = 0; i < Lista.size(); i++) {
 			System.out.println("DADOS: " + Lista.get(i));
 
 		}
-		System.out.println("DADOS da ultima pos: " + Lista.get(4));
+		System.out.println("DADOS da ultima pos: " + Lista.get(5));
+		System.out.println("----------------------------------------------------------------");
+		
+		for(int j = 0; j < Lista.size(); j++){
+			if(Lista.get(j).getNome().equals("TAM")){
+				System.out.println(Lista.get(j));
+			}
+			
+		}
 	}
 
 }

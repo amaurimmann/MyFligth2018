5c8edb0a46ed2334c655c4e11f876af1b0f13875
16111077 <16111077@w3332046.portoalegre.pucrsnet.br>
2018-03-19 21:28:03 -0300

    iniciando

--- MyFlight-base/src/pucrs/myflight/modelo/GerenciadorCias.java
+++ MyFlight-base/src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -8,4 +8,31 @@ public class GerenciadorCias {
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
 	}
+
+	public void adicionar(CiaAerea cia) {
+	    empresas.add(cia);
+    }
+
+    public CiaAerea buscarCodigo(String cod) {
+        for (CiaAerea cia: empresas)
+            if (cod.equals(cia.getCodigo()))
+                return cia;
+        // Não encontrou, retorna null
+        return null;
+    }
+
+    public CiaAerea buscarNome(String nome) {
+        for (CiaAerea cia: empresas)
+            if (nome.equals(cia.getNome()))
+                return cia;
+        // Não encontrou, retorna null
+        return null;
+    }
+
+    public ArrayList<CiaAerea> listarTodas() {
+	    ArrayList<CiaAerea> nova = new ArrayList<>();
+	    for(CiaAerea cia: empresas)
+	        nova.add(cia);
+        return nova;
+    }
 }

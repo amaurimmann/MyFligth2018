84440dd8863067a0e726e2fd7e1c4fff1e0ae4c0
16111077 <16111077@w1847037.portoalegre.pucrsnet.br>
2018-03-20 20:59:23 -0300

    teste com versão de colega link URL

--- MyFlight-base/src/pucrs/myflight/modelo/App.java
+++ MyFlight-base/src/pucrs/myflight/modelo/App.java
@@ -9,6 +9,8 @@ public class App {
 		System.out.println("Teste 02");
 		
 		System.out.println("teste 03");
+		
+		System.out.println("https://github.com/matheusmorcinek/MyFlightPOO/blob/master/MyFlight-base/src/pucrs/myflight/modelo/Aeronave.java");
 
 		
 	}
